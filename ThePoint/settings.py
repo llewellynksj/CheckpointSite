5c48db3028f150b5a761c6525a5214bfd00d3563
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2023-04-15 15:58:13 +0000

    add xframeoptions

--- ThePoint/settings.py
+++ ThePoint/settings.py
@@ -196,6 +196,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
